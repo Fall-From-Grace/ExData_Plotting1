369a305cf986293e1cae6379d4c84358993ba216
Chet Manly <fall-from-grace@Starmoon.(none)>
2014-05-10 12:03:56 -0400

    Comments

--- plot1.R
+++ plot1.R
@@ -1,6 +1,12 @@
+# Author: Fall-From-Grace
+# Date: 10 May 2014
+# Exploratory Data Analysis
+
+#read and subset data
 powerData <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
 powerDataFeb <- subset(powerData, powerData$Date == "1/2/2007" | powerData$Date == "2/2/2007")
 
+#make the graph
 png(filename = "plot1.png", height=480, width=480)
 hist(as.numeric(powerDataFeb$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
