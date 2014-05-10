6bb3ce6ea0283f9abd6abd0da4e643b14104245f
Chet Manly <fall-from-grace@Starmoon.(none)>
2014-05-10 12:10:35 -0400

    Moar Comments

--- plot3.R
+++ plot3.R
@@ -1,6 +1,12 @@
+# Author: Fall-From-Grace
+# Date: 10 May 2014
+# Exploratory Data Analysis
+
+#read and subset data
 powerData <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
 powerDataFeb <- subset(powerData, powerData$Date == "1/2/2007" | powerData$Date == "2/2/2007")
 
+#make the graph
 png(filename = "plot3.png", height=480, width=480)
 
 plot(strptime(paste(powerDataFeb$Date, powerDataFeb$Time), "%d/%m/%Y %H:%M:%S"), as.numeric(powerDataFeb$Sub_metering_1), type="l", ylab = "Energy sub metering", xlab = "")
