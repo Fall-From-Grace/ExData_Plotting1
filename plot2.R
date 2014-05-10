2e23426f5ee24b6cd228cff5a8c62c0f8bbeb32e
Fall-From-Grace <ceirante@gmail.com>
2014-05-10 11:49:07 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,6 @@
+powerData <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+powerDataFeb <- subset(powerData, powerData$Date == "1/2/2007" | powerData$Date == "2/2/2007")
+
+png(filename = "plot2.png", height=480, width=480)
+plot(strptime(paste(powerDataFeb$Date, powerDataFeb$Time), "%d/%m/%Y %H:%M:%S"), as.numeric(powerDataFeb$Global_active_power), type="l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.off()
